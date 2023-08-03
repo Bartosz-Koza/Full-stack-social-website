56773e0ae46aeb75eba54849158b0b5c361eae23
KartoszBoza <Bartoszkoza09@gmail.com>
2023-08-03 17:51:12 +0200

    widgets

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,66 @@
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBorderOutlines,
+    FavoriteOutlined,
+    ShareOutlined
+} from '@mui/icons-material'
+import { Box, Divider, IconButton, Typography, useTheme } from '@mui/material'
+import FlexBetween from '../../components/FlexBetween'
+import Friend from "../../components/Friend"
+import WidgetWrapper from "../../components/WidgetWrapper"
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setPost } from '../../state'
+
+
+const PostWidget =({
+    postId,
+    postUserId,
+    name,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments,
+}) => {
+    const [isComments, setIsComments] = useState(false)
+    const dispatch = useDispatch();
+    const { palette} = useTheme()
+    const token = useSelector((state) => state.token)
+    const loggedInUserId = useSelector((state) => state.user._id)
+    const isLiked = Boolean(likes[loggedInUserId])
+    const likeCount = Object.keys(likes).length
+
+    const main = palette.primary.main
+    const medium = palette.neutral.medium
+
+    const patchLike= async () => {
+        const response = await fetch(`http://localhost3001/posts/${postId}/like`, {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ userId: loggedInUserId})
+        })
+        const updatedPost = await response.json()
+        dispatch(setPost({post: updatedPost}))
+    }
+
+    return (
+        <WidgetWrapper m="2rem 0">
+            <Friend 
+            friendId={postUserId}
+            name={name}
+            subtitle={location}
+            picturePath={picturePath}
+            />
+            <Typography color={main} sx={{ mt: "1rem"}}>
+                {description}
+            </Typography>
+        </WidgetWrapper>
+    )
+}
+
+export default PostWidget
